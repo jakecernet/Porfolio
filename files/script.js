656c5c1b059a2fac8053a3052e134049af44e06e
Jaka Černetič <jaka.cernetic@students.vegova.si>
2024-11-27 19:01:01 +0100

    zafrkancija s pušcicami

--- files/script.js
+++ files/script.js
@@ -8,13 +8,27 @@ window.addEventListener("scroll", function () {
 });
 
 function expand(id) {
+	closeAll(id);
 	var e = document.getElementById(id);
 	var arrow = document.getElementById(id + "arrow");
 	if (e.style.height == "0px" || e.style.height == "") {
-		e.style.height = "auto";
-		arrow.style.transform = "rotate(180deg)";
+		e.style.height = "10rem";
+		arrow.style.transform = "rotate(-90deg)";
 	} else {
 		e.style.height = "0px";
 		arrow.style.transform = "rotate(0deg)";
+	}
+}
+
+function closeAll(exceptId) {
+	var elements = document.getElementsByClassName("liContents");
+	var arrows = document.getElementsByClassName("arrow");
+	for (var i = 0; i < elements.length; i++) {
+		if (elements[i].id != exceptId) {
+			elements[i].style.height = "0px";
+		}
+		if (arrows[i]) {
+			arrows[i].style.transform = "rotate(0deg)";
+		}
 	}
 }
