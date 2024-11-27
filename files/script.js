a2435ddd80c7fa880a5150aa98722baa914ae960
Jaka Černetič <jaka.cernetic@gmail.com>
2024-11-27 14:19:07 +0100

    nov stil za projekte

--- files/script.js
+++ files/script.js
@@ -6,3 +6,15 @@ window.addEventListener("scroll", function () {
 		t = document.querySelector(".top");
 	o > 400 ? (t.style.display = "flex") : (t.style.display = "none");
 });
+
+function expand(id) {
+	var e = document.getElementById(id);
+	var arrow = document.getElementById(id + "arrow");
+	if (e.style.height == "0px" || e.style.height == "") {
+		e.style.height = "auto";
+		arrow.style.transform = "rotate(180deg)";
+	} else {
+		e.style.height = "0px";
+		arrow.style.transform = "rotate(0deg)";
+	}
+}
