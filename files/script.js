d47bfee252bd6ac8496c71b83af679f43d21959b
Jaka Černetič <jaka.cernetic@students.vegova.si>
2024-11-29 16:09:18 +0100

    kr done

--- files/script.js
+++ files/script.js
@@ -11,7 +11,7 @@ function expand(id) {
 	closeAll(id);
 	var e = document.getElementById(id);
 	var arrow = document.getElementById(id + "arrow");
-	if (e.style.height == "0px" || e.style.height == "") {
+	if (e.style.height === "0px" || e.style.height === "") {
 		e.style.height = "10rem";
 		arrow.style.transform = "rotate(-90deg)";
 	} else {
@@ -24,11 +24,25 @@ function closeAll(exceptId) {
 	var elements = document.getElementsByClassName("liContents");
 	var arrows = document.getElementsByClassName("arrow");
 	for (var i = 0; i < elements.length; i++) {
-		if (elements[i].id != exceptId) {
+		var currentId = elements[i].id;
+		if (currentId !== exceptId) {
 			elements[i].style.height = "0px";
+			var currentArrow = document.getElementById(currentId + "arrow");
+			if (currentArrow) {
+				currentArrow.style.transform = "rotate(0deg)";
+			}
 		}
+	}
+}
+
+// Initialize all elements to be closed on page load
+document.addEventListener("DOMContentLoaded", function () {
+	var elements = document.getElementsByClassName("liContents");
+	var arrows = document.getElementsByClassName("arrow");
+	for (var i = 0; i < elements.length; i++) {
+		elements[i].style.height = "0px";
 		if (arrows[i]) {
 			arrows[i].style.transform = "rotate(0deg)";
 		}
 	}
-}
+});
